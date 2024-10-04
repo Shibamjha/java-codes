2150e4d47052d6a05c3896152a911a4a5e1250c0
Shibam jha <103352928+Shibamjha@users.noreply.github.com>
2024-10-04 23:24:49 +0530

    java practice codes

---
+++ Animal.java
@@ -0,0 +1,13 @@
+class Animal 
+{
+public void run()
+{
+System.out.println("I am eating:");
+}
+public static void main(String args[])
+{
+System.out.println("1");
+Animal buzo=new Animal();
+buzo.eat();
+}
+}
